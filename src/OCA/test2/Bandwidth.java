179b1b93f32ee56c70f9b8bcd778c05ffbbe627a
SoiBac1001 <congheit196@gmail.com>
2020-08-22 10:21:11 +0700

    test OCA

--- src/OCA/test2/Bandwidth.java
+++ src/OCA/test2/Bandwidth.java
@@ -3,6 +3,7 @@ package OCA.test2;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.function.Predicate;
 
 public class Bandwidth {
     public int available = 0;
@@ -30,6 +31,7 @@ public class Bandwidth {
     }
 
     public static void main(String[] args) {
+        testString();
         testDate();
         System.out.println("hello" + new String("world"));
         System.out.println(new StringBuilder("world").insert(0, "hello").toString());
@@ -45,10 +47,32 @@ public class Bandwidth {
 
     private static void testDate(){
 //        LocalDateTime greatDay = LocalDateTime.parse("2015-01-01"); // DateTimeParseException
+        Bandwidth bandwidth = new Bandwidth(6);
+        System.out.println(validateBandwidth(bandwidth, bandwidth1 -> bandwidth1.available < 5));
+        String s = "hello";
+        System.out.println(s.replace('h', 's'));
+        System.out.println("hello" + new String("world"));
+        s.length();
         LocalDateTime greatDay2 = LocalDateTime.parse("2007-12-03T10:15:30");
         LocalDate parse = LocalDate.parse("2015-01-01");
         System.out.println(parse);
         String format = greatDay2.format(DateTimeFormatter.ISO_DATE_TIME);
+        System.out.println(DateTimeFormatter.ISO_ZONED_DATE_TIME.format(greatDay2));
         System.out.println(format);
+    }
+
+    private static boolean validateBandwidth(Bandwidth b, Predicate<Bandwidth> p){
+        return p.test(b);
+    }
+
+    private static void testString(){
+        String fullPhoneNumber = "123-456-7890";
+        String mask = "xxx-xxx-";
+        mask.substring(2); // x-xxx-
+        mask.substring(2, 6); //
+        StringBuilder stringBuilder = new StringBuilder("xxx-xxx-");
+        System.out.println(new StringBuilder("xxx-xxx-") + fullPhoneNumber.substring(8));
+        new StringBuilder(fullPhoneNumber).replace(0,7, mask);
+        System.out.println(mask + fullPhoneNumber.substring(8, 12));
     }
 }
