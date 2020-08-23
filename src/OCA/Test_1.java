b0d7b18d9f66362804a3fb6b4ad1afbc2ddadc1b
SoiBac1001 <congheit196@gmail.com>
2020-08-24 00:14:03 +0700

    update on 24-08

---
+++ src/OCA/Test_1.java
@@ -0,0 +1,18 @@
+package OCA;
+
+public class Test_1 {
+}
+
+class Main{
+    private int i = 1;
+    public static void main(String argv[]){
+        float f=3;
+        byte $b=127;
+        int _i = 2;
+        Main s = new Main ();
+        s.someMethod();
+    }
+    public static void someMethod(){
+//        System.out.println(i);
+    }
+}
