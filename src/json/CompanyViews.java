c699102fe2ec6297586d7ed72de9608255a5619c
LDAP\nvbao2 <baonv1001@gmail.com>
2021-11-17 18:24:02 +0700

    test @JsonView

---
+++ src/json/CompanyViews.java
@@ -0,0 +1,9 @@
+package json;
+
+public class CompanyViews {
+    public static class Normal{};
+
+    public static class Manager extends Normal{};
+
+    public static class HR extends Normal{};
+}
