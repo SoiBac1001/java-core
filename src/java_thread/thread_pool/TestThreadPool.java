69355921341f8b2e69bf2914195094b03b0c5aa4
LDAP\nvbao2 <baonv1001@gmail.com>
2021-11-29 18:44:20 +0700

    test thread pool

---
+++ src/java_thread/thread_pool/TestThreadPool.java
@@ -0,0 +1,22 @@
+package java_thread.thread_pool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class TestThreadPool {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(5); //creating a pool of 5 threads
+        for(int i = 0; i < 10; i++) {
+            Runnable worker = new WorkerThread("" + i);
+            executorService.execute(worker); //calling execute method of ExecutorService
+        }
+
+        executorService.shutdown();
+
+        while (executorService.isTerminated()) {
+
+        }
+
+        System.out.println("Finished all threads");
+    }
+}
