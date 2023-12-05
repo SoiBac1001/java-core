a38f306d99c5cf2718e332f18dc0ee9cf94b363b
SoiBac1001 <congheit196@gmail.com>
2023-12-05 17:05:25 +0700

    [dev] implement binary tree

---
+++ src/algorithms/tree/MyBinaryTree.java
@@ -0,0 +1,17 @@
+package algorithms.tree;
+
+public class MyBinaryTree {
+    private TreeNode root;
+
+    public MyBinaryTree() {
+    }
+
+    public void init() {
+        TreeNode n0 = new TreeNode(0);
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n4 = new TreeNode(4);
+        TreeNode n5 = new TreeNode(5);
+    }
+}
---
+++ src/algorithms/tree/TestTree.java
@@ -0,0 +1,7 @@
+package algorithms.tree;
+
+public class TestTree {
+    public static void main(String[] args) {
+
+    }
+}
